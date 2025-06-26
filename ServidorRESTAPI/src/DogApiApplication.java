22d1b23f85467e0d78e74dd904d3c9e6fcd29ce8
SamuelCFalcao <samuelfalcao@alu.ufc.br>
2025-06-26 20:20:42 -0300

    Updated ServidorRestAPI

---
+++ ServidorRESTAPI/src/DogApiApplication.java
@@ -0,0 +1,8 @@
+@SpringBootApplication
+public class DogApiApplication 
+{
+    public static void main(String[] args) 
+    {
+        SpringApplication.run(DogApiApplication.class, args);
+    }
+}
